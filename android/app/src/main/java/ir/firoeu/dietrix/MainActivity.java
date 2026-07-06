7f8ebe00c41abc5cea4ce9bbd7150e514b55111b
firoeu-del <firoeu@gmail.com>
2026-07-06 06:15:52 +0330

    Add Android WebView project (Dietrix) and remaining web assets

---
+++ android/app/src/main/java/ir/firoeu/dietrix/MainActivity.java
@@ -0,0 +1,32 @@
+package ir.firoeu.dietrix;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
